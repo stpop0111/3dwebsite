37a2859b762600c83e569d7a6849776ccfec89bb
stpop0111 <stpop0111@gmail.com>
2024-12-19 01:08:58 +0900

    from main

---
+++ js/model.js
@@ -0,0 +1,16 @@
+export const MODEL_DATA = {
+    vendingMachine: {
+        id: 'vendingMachine',
+        path: '..models/model01_vendingMachine.glb',
+        title: '自動販売機',
+        description: 'どこにでもある普通の自動販売機',
+        displayName: '自販機'
+    },
+    registar: {
+        id: 'vendingMachine',
+        path: '../models/vendingMachine.glb',
+        title: '自動販売機',
+        description: 'どこにでもある普通の自動販売機',
+        displayName: '自販機'
+    },
+}
